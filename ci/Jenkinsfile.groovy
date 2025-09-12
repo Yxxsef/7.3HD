f2695c520fbf641d92f1ccfa3dc451674fea3695
Yousef <y.khalifa888@gmail.com>
2025-09-12 11:31:31 +1000

    HD cleanup: add buildDiscarder + skipDefaultCheckout

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -1,8 +1,16 @@
 pipeline {
   agent any
-  options { timestamps() }
+  options {
+    timestamps()
+    buildDiscarder(logRotator(numToKeepStr: '10'))
+    skipDefaultCheckout(true)
+  }
   stages {
     stage('Checkout'){ steps { checkout scm } }
-    stage('Build'){ steps { sh 'echo OK' } }
+    stage('Build'){ steps { sh 'echo build' } }
+    stage('Test'){ steps { sh 'echo test' } }
+  }
+  post {
+    always { archiveArtifacts artifacts: '**/dist/**, **/build/**', fingerprint: true }
   }
 }
