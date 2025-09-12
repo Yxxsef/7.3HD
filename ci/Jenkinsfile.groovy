baeb3cc5e123e95268d7f29f0d117175a0efc9ef
Yousef <y.khalifa888@gmail.com>
2025-09-12 11:33:56 +1000

    Pipeline: allowEmptyArchive + Package stage to produce dist artifact

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -9,8 +9,13 @@ pipeline {
     stage('Checkout'){ steps { checkout scm } }
     stage('Build'){ steps { sh 'echo build' } }
     stage('Test'){ steps { sh 'echo test' } }
+    stage('Package'){ steps { sh 'mkdir -p dist && echo build > dist/artifact.txt' } }
   }
   post {
-    always { archiveArtifacts artifacts: '**/dist/**, **/build/**', fingerprint: true }
+    always {
+      archiveArtifacts artifacts: 'dist/**,build/**',
+                       fingerprint: true,
+                       allowEmptyArchive: true
+    }
   }
 }
