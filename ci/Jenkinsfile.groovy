29c3b2ee73b8709dc133848ee52c17c9ffdd0526
Yousef <y.khalifa888@gmail.com>
2025-09-13 13:45:42 +1000

    Add Sonar stage and sonar-project.properties

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -8,7 +8,20 @@ pipeline {
   stages {
     stage('Checkout'){ steps { checkout scm } }
     stage('Build'){ steps { sh 'echo build' } }
-    stage('Test'){ steps { sh 'echo test' } }
+    stage('Test'){  steps { sh 'echo test' } }
+    stage('Sonar') {
+      environment { SCANNER_HOME = tool 'SonarScanner' }
+      steps {
+        withSonarQubeEnv('7.3HD') {
+          sh """
+            ${SCANNER_HOME}/bin/sonar-scanner \
+              -Dsonar.projectBaseDir=. \
+              -Dsonar.sources=. \
+              -Dsonar.exclusions=node_modules/**,dist/**,**/*.test.js,**/*.spec.js
+          """
+        }
+      }
+    }
     stage('Package'){ steps { sh 'mkdir -p dist && echo build > dist/artifact.txt' } }
   }
   post {
